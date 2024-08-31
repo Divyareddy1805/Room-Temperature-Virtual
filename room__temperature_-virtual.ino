5e109fd20e289487f22afb7ac08561e215b7903f
Divyareddy1805 <divyareddy182512@gmail.com>
2024-08-31 16:50:27 +0530

    Add files via upload

---
+++ room__temperature_-virtual.ino
@@ -0,0 +1,17 @@
+int val = 0;
+float mv = 0, cel = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A5,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = analogRead(A5);
+Serial.println(val);
+mv = ( val/1024.0)*5000;
+cel = mv/10;
+Serial.println(cel);
+delay(1000);
+}
